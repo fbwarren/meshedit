34739f88dd2e187c7dff5816c9f3bcdf1a5ca87e
fbwarren <fbwarren@berkeley.edu>
2021-03-02 23:43:43 -0600

    finished part 5

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -168,7 +168,99 @@ namespace CGL
     // TODO Part 5.
     // This method should split the given edge and return an iterator to the newly inserted vertex.
     // The halfedge of this vertex should point along the edge that was split, rather than the new edges.
-    return VertexIter();
+      if (e0->isBoundary())
+      { return VertexIter(); }
+
+      // Define halfedges
+      HalfedgeIter h0 = e0->halfedge();
+      HalfedgeIter h1 = h0->next();
+      HalfedgeIter h2 = h1->next();
+      HalfedgeIter h3 = h0->twin();
+      HalfedgeIter h4 = h3->next();
+      HalfedgeIter h5 = h4->next();
+      HalfedgeIter h6 = h1->twin();
+      HalfedgeIter h7 = h2->twin();
+      HalfedgeIter h8 = h4->twin();
+      HalfedgeIter h9 = h5->twin();
+      HalfedgeIter h10 = newHalfedge();
+      HalfedgeIter h11 = newHalfedge();
+      HalfedgeIter h12 = newHalfedge();
+      HalfedgeIter h13 = newHalfedge();
+      HalfedgeIter h14 = newHalfedge();
+      HalfedgeIter h15 = newHalfedge();
+
+      // Define faces
+      FaceIter f0 = h0->face();
+      FaceIter f1 = h3->face();
+      FaceIter f2 = newFace();
+      FaceIter f3 = newFace();
+      if (f0->isBoundary() || f1->isBoundary())
+      { return VertexIter(); }
+
+      // Define vertices
+      VertexIter v0 = h0->vertex();
+      VertexIter v1 = h3->vertex();
+      VertexIter v2 = h6->vertex();
+      VertexIter v3 = h5->vertex();
+      VertexIter v4 = newVertex();
+
+      // Define edges
+      EdgeIter e1 = h1->edge();
+      EdgeIter e2 = h2->edge();
+      EdgeIter e3 = h4->edge();
+      EdgeIter e4 = h5->edge();
+      EdgeIter e5 = newEdge();
+      EdgeIter e6 = newEdge();
+      EdgeIter e7 = newEdge();
+
+      // Set each next, twin, vertex, edge, and face for each halfedge
+      h0->setNeighbors(h1, h3, v0, e6, f0);
+      h1->setNeighbors(h2, h15, v4, e5, f0);
+      h2->setNeighbors(h0, h7, v2, e2, f0);
+
+      h3->setNeighbors(h4, h0, v4, e6, f1);
+      h4->setNeighbors(h5, h8, v0, e3, f1);
+      h5->setNeighbors(h3, h10, v3, e7, f1);
+
+      // Set values for outer halfedges
+      h6->setNeighbors(h6->next(), h14, v2, e1, h6->face());
+      h7->setNeighbors(h7->next(), h2, v0, e2, h7->face());
+      h8->setNeighbors(h8->next(), h4, v3, e3, h8->face());
+      h9->setNeighbors(h9->next(), h11, v1, e4, h9->face());
+
+      h10->setNeighbors(h11, h5, v4, e7, f2);
+      h11->setNeighbors(h12, h9, v3, e4, f2);
+      h12->setNeighbors(h10, h13, v1, e0, f2);
+
+      h13->setNeighbors(h14, h12, v4, e0, f3);
+      h14->setNeighbors(h15, h6, v1, e1, f3);
+      h15->setNeighbors(h13, h1, v2, e5, f3);
+
+      // Set vertices
+      v0->halfedge() = h4;
+      v1->halfedge() = h14;
+      v2->halfedge() = h2;
+      v3->halfedge() = h11;
+      v4->halfedge() = h13;
+      v4->position = lerp(v0->position, v1->position, 0.5);
+
+      // Edges:
+      e0->halfedge() = h13;
+      e1->halfedge() = h14;
+      e2->halfedge() = h2;
+      e3->halfedge() = h4;
+      e4->halfedge() = h11;
+      e5->halfedge() = h1;
+      e6->halfedge() = h0;
+      e7->halfedge() = h5;
+
+      // Faces:
+      f0->halfedge() = h0;
+      f1->halfedge() = h3;
+      f2->halfedge() = h10;
+      f3->halfedge() = h13;
+
+      return v4;
   }
 
   Vector2D lerp(Vector2D p0, Vector2D p1, float t) {
